5159979acee838cb2a68fedb209c8dc39c3b5689
root <root@kserv>
2022-05-09 17:04:26 +0200

    working sql template

--- phymo.sql
+++ phymo.sql
@@ -12,42 +12,20 @@ SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `phymo`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `accounts`
---
+CREATE DATABASE IF NOT EXISTS phymo;
+USE phymo;
 
 CREATE TABLE `accounts` (
   `id` int NOT NULL,
   `password` varchar(30) NOT NULL,
-  `name` varchar(15) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `login` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `name` varchar(15) NOT NULL,
+  `login` varchar(20)  NOT NULL,
   `email` varchar(320) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ;
 
---
--- Dumping data for table `accounts`
---
 
 INSERT INTO `accounts` (`id`, `password`, `name`, `login`, `email`) VALUES
 (1, 'password1', 'user01', 'user01login', '');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `post`
---
 
 CREATE TABLE `post` (
   `id` int NOT NULL,
@@ -57,41 +35,18 @@ CREATE TABLE `post` (
   `likes` int NOT NULL,
   `temporality` datetime NOT NULL,
   `parentid` int DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ;
 
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `accounts`
---
 ALTER TABLE `accounts`
   ADD PRIMARY KEY (`id`);
 
---
--- Indexes for table `post`
---
 ALTER TABLE `post`
   ADD PRIMARY KEY (`id`);
 
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `accounts`
---
 ALTER TABLE `accounts`
   MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
---
--- AUTO_INCREMENT for table `post`
---
 ALTER TABLE `post`
   MODIFY `id` int NOT NULL AUTO_INCREMENT;
 COMMIT;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
