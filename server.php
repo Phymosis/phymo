d20f306fb634c7047a63f817459edb281a369402
jim soulier <jimsoulier8@gmail.com>
2022-05-13 11:57:59 +0200

    ComSystemFinished1

--- server.php
+++ server.php
@@ -103,6 +103,7 @@ if (isset($_POST['reg_user'])) {
   	mysqli_query($db, $query);
   	$_SESSION['username'] = $username;
   	$_SESSION['success'] = "You are registered !";
+	//echo $query;
   	header('location: index.php');
   }
 }
